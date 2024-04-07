ffbdff929ce1263b9d7f108b38b773dad1af4bf0
Planisph3r3 <ennzopz@gmail.com>
2024-04-07 03:25:38 +0000

    Fix: Wrong Argv declaration

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const request = require('request')
+const request = require('request');
 
-const urlArg = process.argv[1]
+const urlArg = process.argv[2];
 
 request
   .get(urlArg)
   .on('response', function (response) {
-    console.log(`code: ${response.statusCode}`)
-  })
+    console.log(`code: ${response.statusCode}`);
+  });
