ffaa113fbc4a137934ab050303d99f6cc8b99c76
ubuvm <ubuvm@UbuVm.myguest.virtualbox.org>
2024-04-06 21:09:24 -0500

    Reads and prints the content of a file

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs')
+
+const filePath = process.argv[1]
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err)
+    return
+  }
+
+  console.log(data)
+})
