46496db7749f548c416372b69cfdb52ea941bca6
Planisph3r3 <ennzopz@gmail.com>
2024-04-07 03:26:26 +0000

    Fix: Wrong Argv declaration

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,34 +1,34 @@
 #!/usr/bin/node
 
-const request = require('request')
+const request = require('request');
 
-const promptArg = process.argv[2]
+const promptArg = process.argv[2];
 
 request(promptArg, (error, response, body) => {
   if (error) {
-    console.log('Error on request')
-    return
+    console.log('Error on request');
+    return;
   }
   if (response.statusCode !== 200) {
-    console.log(`Failed to retreive data. code: ${response.statusCode}`)
-    return
+    console.log(`Failed to retreive data. code: ${response.statusCode}`);
+    return;
   }
-  const objJson = JSON.parse(body)
+  const objJson = JSON.parse(body);
   function todosIdentifier (objJson) {
-    let todosCounter = 0
-    let idRaiser = 1
-    const objStore = {}
+    let todosCounter = 0;
+    let idRaiser = 1;
+    const objStore = {};
     for (const idx of objJson) {
       if (idx.completed === true && idx.userId === idRaiser) {
-        todosCounter += 1
+        todosCounter += 1;
       } else if (idx.userId !== idRaiser) {
-        objStore[idRaiser] = todosCounter
-        idRaiser += 1
-        todosCounter = 0
+        objStore[idRaiser] = todosCounter;
+        idRaiser += 1;
+        todosCounter = 0;
       }
-    };
-    objStore[idRaiser] = todosCounter
-    console.log(objStore)
+    }
+    objStore[idRaiser] = todosCounter;
+    console.log(objStore);
   }
-  todosIdentifier(objJson)
-})
+  todosIdentifier(objJson);
+});
