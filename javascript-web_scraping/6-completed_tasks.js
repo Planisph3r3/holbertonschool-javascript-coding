da4d09e52ad01bda10392bd4af601cba57bbf0f6
Planisph3r3 <ennzopz@gmail.com>
2024-04-07 04:02:36 +0000

    Fix: Corrected logic

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -2,33 +2,37 @@
 
 const request = require('request');
 
-const promptArg = process.argv[2];
+const apiUrl = process.argv[2];
 
-request(promptArg, (error, response, body) => {
+request(apiUrl, (error, response, body) => {
   if (error) {
-    console.log('Error on request');
+    console.error('Error on request:', error);
     return;
   }
   if (response.statusCode !== 200) {
-    console.log(`Failed to retreive data. code: ${response.statusCode}`);
+    console.error(`Failed to retrieve data. Code: ${response.statusCode}`);
     return;
   }
+
   const objJson = JSON.parse(body);
-  function todosIdentifier (objJson) {
-    let todosCounter = 0;
-    let idRaiser = 1;
-    const objStore = {};
-    for (const idx of objJson) {
-      if (idx.completed === true && idx.userId === idRaiser) {
-        todosCounter += 1;
-      } else if (idx.userId !== idRaiser) {
-        objStore[idRaiser] = todosCounter;
-        idRaiser += 1;
+  const objStore = {};
+  let todosCounter = 0;
+  let idRaiser = 1;
+
+  for (const idx of objJson) {
+    if (idx.completed === true && idx.userId === idRaiser) {
+      todosCounter++;
+    } else if (idx.userId !== idRaiser) {
+      objStore[idRaiser] = todosCounter;
+      idRaiser++;
+      if (idx.completed) {
+        todosCounter = 1;
+      } else {
         todosCounter = 0;
       }
     }
-    objStore[idRaiser] = todosCounter;
-    console.log(objStore);
   }
-  todosIdentifier(objJson);
+  objStore[idRaiser] = todosCounter;
+
+  console.log(objStore);
 });
