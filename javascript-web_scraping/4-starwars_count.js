a19167fc76f754dbb8cbb24a6cff03a7d7237c69
ubuvm <ubuvm@UbuVm.myguest.virtualbox.org>
2024-04-06 21:30:25 -0500

    Printing the # of times a character is on a movie

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const request = require('request')
+
+const promptArg = process.argv[1]
+
+request(promptArg, (error, response, body) => {
+  if (error) {
+    console.log('Error on request')
+    return
+  }
+  if (response.statusCode !== 200) {
+    console.log(`Failed to retreive data. code: ${response.statusCode}`)
+    return
+  }
+  // extraccct
+  const objJson = JSON.parse(body)
+  function characterCount (objJson) {
+    let realcharCount = 0
+    const filmCount = objJson.count
+    for (let idx = 0; idx < filmCount; idx++) {
+      for (const item of objJson.results[idx].characters) {
+        if (item === 'https://swapi-api.hbtn.io/api/people/18/') {
+          realcharCount += 1
+        }
+      }
+    }
+    console.log(realcharCount)
+  }
+  characterCount(objJson)
+})
