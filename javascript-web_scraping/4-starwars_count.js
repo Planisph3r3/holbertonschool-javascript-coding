2ff9eace0c117860db2202ff8226b409d59becda
Planisph3r3 <ennzopz@gmail.com>
2024-04-07 03:26:17 +0000

    Fix: Wrong Argv declaration

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,31 +1,31 @@
 #!/usr/bin/node
 
-const request = require('request')
+const request = require('request');
 
-const promptArg = process.argv[1]
+const promptArg = process.argv[2];
 
 request(promptArg, (error, response, body) => {
   if (error) {
-    console.log('Error on request')
-    return
+    console.log('Error on request');
+    return;
   }
   if (response.statusCode !== 200) {
-    console.log(`Failed to retreive data. code: ${response.statusCode}`)
-    return
+    console.log(`Failed to retreive data. code: ${response.statusCode}`);
+    return;
   }
   // extraccct
-  const objJson = JSON.parse(body)
+  const objJson = JSON.parse(body);
   function characterCount (objJson) {
-    let realcharCount = 0
-    const filmCount = objJson.count
+    let realcharCount = 0;
+    const filmCount = objJson.count;
     for (let idx = 0; idx < filmCount; idx++) {
       for (const item of objJson.results[idx].characters) {
         if (item === 'https://swapi-api.hbtn.io/api/people/18/') {
-          realcharCount += 1
+          realcharCount += 1;
         }
       }
     }
-    console.log(realcharCount)
+    console.log(realcharCount);
   }
-  characterCount(objJson)
-})
+  characterCount(objJson);
+});
--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,33 +1,33 @@
 #!/usr/bin/node
 
-const request = require('request')
+const request = require('request');
 
-const promptArg = process.argv[1]
+const promptArg = process.argv[2];
 
 request(promptArg, (error, response, body) => {
   if (error) {
-    console.log('Error on request')
-    return
+    console.log('Error on request');
+    return;
   }
   if (response.statusCode !== 200) {
-    console.log(`Failed to retreive data. code: ${response.statusCode}`)
-    return
+    console.log(`Failed to retreive data. code: ${response.statusCode}`);
+    return;
   }
-  writeFile(body)
-})
+  writeFile(body);
+});
 
 function writeFile (body) {
-  const fs = require('fs')
+  const fs = require('fs');
 
-  const filePath = process.argv[2]
+  const filePath = process.argv[3];
 
-  const dataToWrite = body
+  const dataToWrite = body;
 
   fs.writeFile(filePath, dataToWrite, 'utf8', (err) => {
     if (err) {
-      console.error('Error writing file:', err)
-      return
+      console.error('Error writing file:', err);
+      return;
     }
-    console.log('File has been written.')
-  })
+    console.log('File has been written.');
+  });
 }
